1c8580e70d1cdd459e2cc43afd249b40e5849ebd
isalazarjara@outlook.com <Ian Salazar>
2022-08-22 17:05:13 -0400

    models_tweet

---
+++ models/tweet.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+# Pydantic
+from pydantic import BaseModel, Field
+
+from models.user import User
+
+# Models
+
+
+class Tweet(BaseModel):
+    tweet_id: UUID = Field(..., description="Tweet ID")
+    content: str = Field(...,
+                         min_length=1,
+                         max_length=256,
+                         description="Tweet Content")
+    created_at: datetime = Field(default=datetime.now(),
+                                 description="Tweet Created At")
+    updated_at: Optional[datetime] = Field(default=datetime.now())
+    created_by: User = Field(..., description="Tweet Created By")
