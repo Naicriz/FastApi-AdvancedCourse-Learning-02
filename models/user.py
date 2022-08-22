d13e244040f4bf131783aea553cb3f45cb90939f
isalazarjara@outlook.com <Ian Salazar>
2022-08-22 16:49:21 -0400

    models_user

---
+++ models/user.py
@@ -0,0 +1,42 @@
+# Python Native Library
+from datetime import date
+from uuid import UUID
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel, EmailStr, Field
+
+# Models
+
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(..., description="User ID")
+    email: EmailStr = Field(..., description="User Email")
+    username: str = Field(...,
+                          min_length=4,
+                          max_length=20,
+                          description="User Username")
+
+
+class User(UserBase):
+    password: str = Field(...,
+                          min_length=8,
+                          max_length=60,
+                          description="User Password")
+    first_name: str = Field(...,
+                            min_length=4,
+                            max_length=30,
+                            description="User First Name")
+    last_name: str = Field(...,
+                           min_length=4,
+                           max_length=30,
+                           description="User Last Name")
+    birth_day: Optional[date] = Field(default=None,
+                                      description="User Birth Day")
+
+
+class userLogin(UserBase):
+    password: str = Field(...,
+                          min_length=8,
+                          max_length=60,
+                          description="User Password")
