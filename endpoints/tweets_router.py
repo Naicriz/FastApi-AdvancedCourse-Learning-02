7e994eaf6919b8c31a528e6438291fcf1227ccfe
Naicriz <ian.dev@buzon.cl>
2022-08-24 01:51:11 -0400

    tweets_post

--- endpoints/tweets_router.py
+++ endpoints/tweets_router.py
@@ -1,8 +1,9 @@
 # Python
+import json
 from typing import List
 
 # FastAPI
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 
 from models import Tweet
 
@@ -26,8 +27,30 @@ async def show_all_tweets():
                    response_model=Tweet,
                    status_code=201,
                    summary="Post a tweet")
-async def post_tweet():
-    pass
+async def post_tweet(tweet: Tweet = Body(...)):
+    """
+    # **Post a Tweet**
+
+    This endpoint/path operation allows you to post a tweet to the application.
+
+    **Parameters:**\n
+    Request Body:
+    - `tweet: Tweet`
+
+    **Returns:** A json with the basic user information;
+    - `twet_id: UUID`
+    - `content: str`
+    - `created_at: datetime`
+    - `updated_at: datetime`
+    - `updated_by: User`
+    - `birth_date: date`
+    """
+    with open("utils/tweets.json", "r+", encoding="utf-8") as f:
+        results = json.load(f)
+        tweet_dict = tweet.dict()
+        results.append(tweet_dict)
+        f.seek(0)
+        json.dump(results, f, default=str, indent=4)
 
 
 # Show a tweet by ID
